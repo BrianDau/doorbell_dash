92dbf0e9108eb117a6822fc31f1951226572164d
Brian Dauernheim <brian@the-ds.com>
2019-02-02 13:38:13 -0500

    Update ExampleCreds.py

--- ExampleCreds.py
+++ ExampleCreds.py
@@ -1,14 +1,7 @@
 ##PUT ALL INFO BETWEEN QUOTES BELOW AND RENAME THIS FILE TO 'creds.py'
 
-#Twilio API Account info
-ACCOUNT_SID = ""
-AUTH_TOKEN = ""
-
-# your cell phone number below (must begin with '+', example: "+15555555555")
-TO_PHONE = ""
-
-# your Twilio phone number below (must begin with '+', example: "+15555555555")
-FROM_PHONE = ""
+#IFTTT recipe
+IFTTT_URL = 'https://maker.ifttt.com/trigger/{event_name}/with/key/{your_key}'
 
 # Imgur ID
 CLIENT_ID = ""
